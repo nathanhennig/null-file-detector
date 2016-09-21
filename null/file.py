d7b6210012365753d40ce5ad7868793c135a3210
SilverStrange <NathanHennig@gmail.com>
2016-09-20 21:35:09 -0700

    Adds more null.File functionality.

--- null/file.py
+++ null/file.py
@@ -1,4 +1,14 @@
+import os
+
+
 class File:
 
     def __init__(self, name):
         self.name = name
+        self.null_count = 0
+
+        _, self.suffix = os.path.splitext(self.name)
+        if self.suffix and len(self.suffix) > 0 and self.suffix[0] == '.':
+            self.suffix = self.suffix[1:]
+
+        self.size = os.stat(self.name).st_size
