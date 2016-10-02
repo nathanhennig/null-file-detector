ef2c3725c0434c0d2183ea1dd5126e4d43b252bc
SilverStrange <NathanHennig@gmail.com>
2016-10-02 14:05:10 -0700

    Updates num_workers to use one less processor than max available. Adds error catching for files that cannot be scanned (such as pagefile.sys on Windows). Changes scan_target() to use and generate absolute paths.

--- null/scan.py
+++ null/scan.py
@@ -5,6 +5,7 @@ from multiprocessing import Process
 import multiprocessing as mp
 import sys
 import os
+import platform
 
 # https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
 # Module multiprocessing is organized differently in Python 3.4+
@@ -17,7 +18,9 @@ try:
 except ImportError:
     import multiprocessing.forking as forking
 
-if sys.platform.startswith('win'):
+bit64 = platform.architecture()[0].startswith('64')
+
+if sys.platform.startswith('win') and bit64:
     # First define a modified version of Popen.
     class _Popen(forking.Popen):
         def __init__(self, *args, **kw):
@@ -67,22 +70,27 @@ def do_work(in_queue, out_queue, null_char):
 def scan(name, work_queue, result_queue):
 
     # produce data
-    with open(name, 'rb') as f:
-        for i in read_in_chunks(f):
-            work_queue.put(i)
+    try:
+        with open(name, 'rb') as f:
+            for i in read_in_chunks(f):
+                work_queue.put(i)
+    except IOError:
+        return 'Error'
+    else:
+        work_queue.join()
 
-    work_queue.join()
+        # get the results
+        null_count = sum([result_queue.get()
+                          for i in range(result_queue.qsize())])
 
-    # get the results
-    null_count = sum([result_queue.get()
-                      for i in range(result_queue.qsize())])
-
-    return null_count
+        return null_count
 
 
 def create_workers(work_queue, result_queue, null_char=b'\x00'):
 
-    num_workers = mp.cpu_count()
+    num_workers = mp.cpu_count() - 1
+    if num_workers < 1:
+        num_workers = 1
 
     # start workers
     worker_list = []
@@ -96,14 +104,17 @@ def create_workers(work_queue, result_queue, null_char=b'\x00'):
 
 def scan_target(path, files, directories):
 
+    path = os.path.abspath(path)
+
     if not os.path.isdir(path):
         files.append(path)
         return files, directories
 
     for entry in os.listdir(path):
-        if os.path.isdir(entry):
-            directories.append(entry)
+        entry_path = os.path.join(path, entry)
+        if os.path.isdir(entry_path):
+            directories.append(entry_path)
         else:
-            files.append(entry)
+            files.append(entry_path)
 
     return files, directories
